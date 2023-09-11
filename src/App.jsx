c6568997545119ef013be7f05dbabcf308b76f1c
Omprakash Prajapati <oprajapati249@gmail.com>
2023-09-11 13:59:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <header>
+      <div>
+        <img src="Logo" alt="" />
+      </div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+
+        <button>Contact</button>
+      </div>
+    </header>
+  );
+}
